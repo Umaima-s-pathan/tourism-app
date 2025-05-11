a26c24048f87f52088c31e4766fe81a52b4f870f
Umaima-s-pathan <105055412+Umaima-s-pathan@users.noreply.github.com>
2025-05-11 21:34:37 +0530

    Update tourism_app.py

--- tourism_app.py
+++ tourism_app.py
@@ -89,16 +89,7 @@ if not monthly_df.empty:
   st.plotly_chart(fig2, use_container_width=True)
 
 # ----------------- Map ----------------
-st.markdown(
-    """
-    <style>
-    iframe[title="streamlit_folium.st_folium"] {
-        height: 500px !important;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+with st.container():
 st.write("### Cultural Hotspot Map")
 map_df = load_data_from_snowflake("SELECT PLACE, STATE, LATITUDE, LONGITUDE FROM HOTSPOT_LOCATIONS")
 if not map_df.empty:

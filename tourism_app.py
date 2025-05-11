8f6d3cd5516033020bc08b111ed8e089c2a4e16f
Umaima-s-pathan <105055412+Umaima-s-pathan@users.noreply.github.com>
2025-05-11 20:27:23 +0530

    Update tourism_app.py

--- tourism_app.py
+++ tourism_app.py
@@ -99,7 +99,7 @@ if not map_df.empty:
             popup=f"{row['PLACE']} ({row['STATE']})",
             tooltip=row['PLACE']
         ).add_to(m)
-    st_folium(m, width=700.height=500)
+    st_folium(m, width=700,height=500)
 
 # ----------------- Hidden Cultural Gems ------------------
 st.subheader("\U0001F3F0 Hidden Cultural Gems")
